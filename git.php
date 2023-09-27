f19e84de51a7908ed892045e26e57e303bfbabc2
Jicxerr <jtj9928@students.uc-bcf.edu.ph>
2023-09-27 16:11:35 +0800

    Initial Commit 3

---
+++ git.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Command</title>
+</head>
+<body>
+    <h2>Git Commands</h2>
+    <br>
+    <h5>Add</h5>
+    <p>
+        git add . <br>
+        git commit -m "Inital Commit"<br>
+        git push origin<br>
+    </p>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,14 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>CIT17</title>
 </head>
 <body>
+    <h1>CIT17 Web Information System</h1>
+    
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="http://github.com">Github</a>
+        <a href="git.php">Git Commands</a>
+    </nav>
+    
     <?php
-        echo "Hellow World";
-        echo "Hellow World";
-        echo "Hellow World";
-        echo "Hellow World";
+        $dirPath = "./";
+        if ($handle = opendir($dirPath)) {
+            echo "<br><br>--- FILES LIST ---<br><br>";
+            while (false !== ($entry = readdir($handle))) {
+                if ($entry != "." && $entry != ".." && is_file($dirPath . '/' . $entry)) {
+                echo "<a href=".$entry.">".$entry."</a>" . "<br>";
+                }
+            }
+            closedir($handle);
+        
+        } else {
+            echo "Failed to open directory.";
+        }
     ?>
+
 </body>
 </html>
