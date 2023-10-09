5a4d3182296b66d3c43f51798c26b6a686bdf6cc
unknown <CIT173B-32@S312.UC-BCF.EDU.PH>
2023-10-09 18:03:38 +0800

    new commit

--- calcu.php
+++ calcu.php
@@ -5,29 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
+<?php
+$first_num = $_POST['first_num'];
+$second_num = $_POST['second_num'];
+$operator = $_POST['operator'];
+$result = '';
+if (is_numeric($first_num) && is_numeric($second_num)) {
+    switch ($operator) {
+        case "Add":
+           $result = $first_num + $second_num;
+            break;
+        case "Subtract":
+           $result = $first_num - $second_num;
+            break;
+        case "Multiply":
+            $result = $first_num * $second_num;
+            break;
+        case "Divide":
+            $result = $first_num / $second_num;
+    }
+}
+
+?>
+
 <body>
-    <h1>Simple Calculator</h1><br>
-    First Number:<input name="n1" value=""><br>
-    Second Number:<input name="n2" value=""><br>
-    <input type="submit" name="sub" value="+">
-    <input type="submit" name="sub" value="-">
-    <input type="submit" name="sub" value="x">
-    <input type="submit" name="sub" value="/"><br>
-    <br>Result: <input type='text' value="<?php echo $ans; ?>"><br>
-    </form>
-    <?php
-    if(isset($_POST['sub'])){
-        $num1=$_POST['n1'];
-        $num2=$_POST['n2'];
-        $oprnd=$_POST['sub'];
-        if($oprnd=="+")
-            $ans=$num1+$num2;
-        else if($oprnd=="-")
-            $ans=$num1-$num2;
-        else if($oprnd=="x")
-            $ans=$num1*$num2;
-        else if($oprnd=="/")
-            $ans=$num1/$num2;
-    }?>
+    <div id="page-wrap">
+        <h1>PHP - Simple Calculator</h1>
+        <form action="" method="post" id="quiz-form">
+                <p>
+                    <input type="number" name="first_num" id="first_num" required="required" value="<?php echo $first_num; ?>" /> <b>First Number</b>
+                </p>
+                <p>
+                    <input type="number" name="second_num" id="second_num" required="required" value="<?php echo $second_num; ?>" /> <b>Second Number</b>
+                </p>
+                <p>
+                    <input readonly="readonly" name="result" value="<?php echo $result; ?>"> <b>Result</b>
+                </p>
+                <input type="submit" name="operator" value="Add" />
+                <input type="submit" name="operator" value="Subtract" />
+                <input type="submit" name="operator" value="Multiply" />
+                <input type="submit" name="operator" value="Divide" />
+        </form>
+    </div>
 </body>
 </html>
