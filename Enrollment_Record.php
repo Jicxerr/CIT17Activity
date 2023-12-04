bacaeaf0d06099ea4937dd4314f9203d5a78979f
Jicxerr <jtj9928@students.uc-bcf.edu.ph>
2023-12-04 18:55:58 +0800

    update delete added

--- Enrollment_Record.php
+++ Enrollment_Record.php
@@ -79,14 +79,14 @@
                 <td><input type="date" id="datepicker" name="selectedDate"></td></tr>
                 <tr><td><label for="fname">Grade:</label></td>
                 <td><input type="number" id="integerInput" name="grade" required></td></tr>      
-                <tr><td></td><td><input type="submit" value="submit" name="submit"></td></tr>
+                <tr><td></td><td><input type="submit" value="submit" name="addenroll"></td></tr>
             </form>
         </table>
-    </div>
+    
     
     <?php 
 
-    if($_SERVER["REQUEST_METHOD"] == "POST")
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['addenroll']))
     {
         try{
             $student = $_POST['student'];
@@ -113,6 +113,7 @@
         
         
     }?>
+    </div>
 
     <div class="card-style">
         <h1>Enrollment Records</h1>
@@ -123,6 +124,7 @@
             <th>Course ID</th>
             <th>Enrollment Date</th>
             <th>Grade</th>
+            <th>Options</th>
         </tr>
         <?php
         $sql = "SELECT * FROM enrollment";
@@ -136,12 +138,93 @@
                 . "<td>" . $row["StudentID"]. "</td>"
                 . "<td>" . $row["CourseID"]. "</td>"
                 . "<td>" . $row["EnrollmentDate"]. "</td>"
-                . "<td>" . $row["Grade"]. "</td>";
+                . "<td>" . $row["Grade"]. "</td>"
+                . "<td><form method=".'POST'.">" 
+                . "<input type=".'hidden'." value=". '_method' ." name= " . "DELETE"  ."/>" 
+                . "<button type=".'submit'." value=". $row["EnrollmentID"] ." name= " . 'deleteButton' .">Delete</button>" 
+                . "</form></td></tr>";
             }
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }?>
         </table>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['deleteButton'])) {
+                $idToDelete = $_POST['deleteButton'];;
+
+                $eenrollsql = "DELETE FROM enrollment WHERE EnrollmentID=$idToDelete";
+
+                if ($conn->query($eenrollsql) === TRUE) {
+                    echo "Record deleted successfully";
+                } else {
+                    echo "Error deleting record: " . $conn->error;
+                }
+        }
+    ?>
+    </div>
+    <div class="card-style">
+        <?php 
+        $selecteditsql = "SELECT EnrollmentID, StudentID, EnrollmentDate, Grade FROM enrollment";
+        $result = $conn->query($selecteditsql);
+        ?>
+        <h1>Edit Records</h1>
+        <form method="POST">
+            <label for="enrolls_id">Select Student</label>
+            <select name="enrolls_id" id="enrolls_id">
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<option value="' . $row['EnrollmentID'] . '">' ."Student:". $row['StudentID'] ." Date:". $row['EnrollmentDate']."  (Grade:". $row['Grade'].") " . '</option>';
+                    }
+                }
+                ?>
+            </select>
+            <input type="submit" value="Show Student Info">
+        </form>
+        <?php
+    // Check if the form has been submitted and a student ID is selected
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['enrolls_id'])) {
+        $selected_id = $_POST['enrolls_id'];
+
+        // Fetch student details based on the selected ID
+        $enrolleditsql = "SELECT * FROM enrollment WHERE EnrollmentID = $selected_id";
+        $result = $conn->query($enrolleditsql);
+
+        if ($result->num_rows > 0) {
+            $editstudent = $result->fetch_assoc();
+            // Display input fields with fetched student information
+            ?>
+            <table style="width:40%">
+            <form method="POST" action="Enrollment_Record.php">
+            <input type="hidden" name="eenrollment_id" value="<?php echo $editstudent['EnrollmentID']; ?>">
+            <tr><td>Enrollment Date:</td>
+             <td><input type="date" name="enrolldates" value="<?php echo $editstudent['EnrollmentDate']; ?>"></td></tr>
+             <tr><td>Grade:</td>
+             <td><input type="text" name="enrollgrade" value="<?php echo $editstudent['Grade']; ?>"></td></tr>
+             <tr><td></td><td><input type="submit" value="Update"></td></tr>
+            </form>
+            </table>
+            <?php
+        } else {
+            echo "No student found with this ID.";
+        }
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['eenrollment_id'])) {
+        $enrollmentids = $_POST['eenrollment_id'];
+        $enrollsdates = $_POST['enrolldates'];
+        $grades = $_POST['enrollgrade'];
+    
+        $eupdatesql = "UPDATE enrollment SET EnrollmentDate='$enrollsdates', Grade='$grades' WHERE EnrollmentID='$enrollmentids'";
+    
+        if ($conn->query($eupdatesql) === TRUE) {
+            echo "Enrollment information updated successfully";
+        } else {
+            echo "Error updating Enrollment information: " . $conn->error;
+        }
+    }
+
+    ?>
     </div>
 </body>
 </html>
